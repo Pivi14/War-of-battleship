335b3e8ee169aa983800c103f76771302574f334
pivarcsi <pivarcsiadam@gmail.com>
2019-11-28 14:46:37 +0100

    rule print added

--- game_rule.py
+++ game_rule.py
@@ -1,3 +1,9 @@
+from function import screen_clear
 
 def game_rule():
-    pass
+    press_message = "Press the Enter"
+    with open("rule.txt", "r") as file:
+        screen_clear()
+        for line in file:
+            print(line.rstrip())
+        input(press_message)
